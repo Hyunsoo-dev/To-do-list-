819acffff8d9ccd9757794dd6ee50635aea8e6c4
Hyunsoo-dev <jing07161@gmail.com>
2020-11-29 15:04:17 +0900

    fix: 일요일이 undefined로 나오는 문제 수정

--- clock.js
+++ clock.js
@@ -5,12 +5,12 @@ function countClock() {
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
   let day = date.getDate();
-  let dayOfWeek = date.getDay() - 1; 
+  let dayOfWeek = date.getDay(); 
   let hours = date.getHours();
 
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-  let DAY_OF_WEEK = ['월', '화', '수', '목', '금', '토', '일'];
+  let DAY_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'];
   if (hours >= 12 && hours <= 24) {
     $clock.textContent = `${year}년 ${month}월 ${day}일 ${DAY_OF_WEEK[dayOfWeek]}요일 오후 ${hours - 12}시 ${minutes}분 ${seconds}초`;  
   } else if (hours >= 1 && hours < 12) {
